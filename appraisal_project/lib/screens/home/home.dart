64600892aac28b8b4bc94a9b09776bf34ec5f8b9
teresa201 <tyienger@smu.edu>
2019-06-27 21:26:22 -0500

    Set up folders and routing
    Routing between the home and forms page works.

---
+++ appraisal_project/lib/screens/home/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:appraisal_project/screens/home/widgets/goappform_button.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Apprasial App'),
+        ),
+        body: Center(
+          child: GoAppFormButton()
+        ),
+      );
+  }
+}
